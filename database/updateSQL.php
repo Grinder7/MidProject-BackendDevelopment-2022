6c348b3c9eafc977f56d43bb17dd21f3e05bc161
Calvin Farrellino Kurniawan <cafaku737@gmail.com>
2023-02-16 15:49:29 +0700

    Update updateSQL.php

--- database/updateSQL.php
+++ database/updateSQL.php
@@ -2,13 +2,23 @@
 
 include "./db_connect.php";
 
-if($data["type"] == "res"){
-    $sql = "UPDATE `tasks` SET status = 0 WHERE tid = ";
+$data = json_decode(file_get_contents("php://input"));
+if (isset($data)) {
+    $tid = $data["tid"];
+    if ($data["type"] == "res") {
+        $sql = "UPDATE `tasks` SET status = 0 WHERE tid = $tid";
+    } else if ($data["type"] == "don") {
+        $sql = "UPDATE `tasks` SET status = 1 WHERE tid = $tid";
+    } else if ($data["type"] == "del") {
+        $sql = "DELETE `tasks` WHERE tid = $tid";
+    }
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        mysqli_close($conn);
+        echo json_encode(["status" => "success", "message" => "Data updated successfully"]);
+    } else {
+        echo json_encode(["status" => "error", "message" => "No data found"]);
+    }
+} else {
+    echo json_encode(["status" => "error", "message" => "No data found"]);
 }
-else if($data["type"] == "don"){
-    $sql = "UPDATE `tasks` SET status = 1 WHERE tid = ";
-}
-else if($data["type"] == "del"){
-    $sql = "DELETE `tasks` where tid = ";
-}
-mysqli_query($conn, $sql);
